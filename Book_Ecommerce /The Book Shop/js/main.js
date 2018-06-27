423237a97fa8d75328abc71fdd5cb985d3f19ed7
Minnacava <marinacamachovarea@outlook.com>
2018-06-27 12:33:14 +0200

    last updated
    best sell javascript fixed

--- Book_Ecommerce /The Book Shop/js/main.js
+++ Book_Ecommerce /The Book Shop/js/main.js
@@ -1,29 +1,34 @@
 
 // --------------------------------HOME PAGE 
-//Best Sell 
-    let books = [
-            { "pic":"img/call_to_action/books2.jpg",
-            "title":"Lalala"
-            },
+//------------Redirect
 
-            { "pic":"img/call_to_action/books.jpg",
-            "title":"lululu"
-            },
+//------------Pages
 
-            { "pic":"img/call_to_action/book-ejemplo3.jpg",
-            "title":"lululu"
-            }
-   ];        
+//------------logo
 
-class bestSell{
-    consructor(books){
+
+//------------Best Sell 
+var books = [
+    { "pic":"img/call_to_action/theNeYorker.jpg",
+    "title":"The New Yorker"},
+
+    { "pic":"img/call_to_action/DesignasArt.jpg",
+    "title":"Design as Art"},
+
+    { "pic":"img/call_to_action/book-ejemplo3.jpg",
+    "title":"Found my Tribe"}
+];   
+
+
+class BestSell{
+    constructor(books){
         this.d1 = document.getElementById("bSell_section");
         this.books = books;
         console.log(books);
-       }
+    }
     showBooks(){
          for(let product of this.books){
-            let div1 = document.createElement ("div");
+            let div1 = document.createElement("div");
             div1.classList.add("col-4", "my-1");
 
             let title = document.createElement("h6");
@@ -34,18 +39,19 @@ class bestSell{
             let pic = document.createElement("img");
             pic.setAttribute("src", product.pic); 
             pic.classList.add("card-image-best-Sell", "img-fluid",  "shadow-sm" );
-            div1.appendChild(pic);
-            
+            div1.appendChild(pic); 
+
+            this.d1.appendChild(div1);
         }
     }
-  
 };
 function loadBestSell(books){
-    let myItem = new bestSell(books);
-     myItem.showBooks();
+    let myItem = new BestSell(books);
+    myItem.showBooks();
 }
+var data = loadBestSell(books);
 
-let data = loadBestSell();
+
  
   
    
