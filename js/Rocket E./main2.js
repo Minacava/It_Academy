5fb605e4f5464bbb6c786379ba1b0c5e7956ae77
Minnacava <marinacamachovarea@outlook.com>
2018-07-24 20:13:06 +0200

    Nueva actualizacion de los metodos Acelerar/Frenar

--- js/Rocket E./main2.js
+++ js/Rocket E./main2.js
@@ -9,23 +9,20 @@ class Rocket {
             return propulsor.potenciaMaxima;
           });
         let velAct = this.propulsores.map(function (propulsor) {
-            return propulsor.velocitatActual;
+            return propulsor.velocidadActual;
           });
         document.write(`${this.identificador}:${propulsorMax}:  ${velAct} </br>`) 
     }
     acelerar(){
         for(var i = 0; i < this.propulsores.length; i++){      
-            if(this.propulsores[i].velocidadActual === this.propulsores[i].potenciaMaxima){
-                return this.propulsores[i].velocidadActual;
-            }else{
+            if(this.propulsores[i].velocidadActual < this.propulsores[i].potenciaMaxima){
                 this.propulsores[i].velocidadActual+=10;
-            }
-           
+            }   
         }
     }
     frenar(){
         for(var i = 0; i < this.propulsores.length; i++){
-            
+            if(this.propulsores[i].velocidadActual >0)
             this.propulsores[i].velocidadActual-=10;
         }
     }    
@@ -40,24 +37,26 @@ let rocket1 = new Rocket("32WESSDS",[new Propulsor(10),new Propulsor(30), new Pr
 let rocket2 = new Rocket("LDSFJA32",[new Propulsor(30), new Propulsor(40), new Propulsor(50), new Propulsor(50), new Propulsor(30), new Propulsor(10)]);
 
 
-
-for (i=0; i < 6; i++){
-    console.log(rocket1.acelerar());
-    console.log(rocket2.acelerar());
+rocket1.info();
+for (i=0; i < 3; i++){
+    rocket1.acelerar();
+    rocket2.acelerar();
 }
     rocket1.info();
     rocket2.info();
-// for (i=0; i < 5; i++){
-//     console.log(rocket1.frenar());
-// }
-// for (i=0; i < 7; i++){
-//     console.log(rocket2.acelerar());
-// }
-//     rocket1.info();
-//     rocket2.info();
-//  for (i=0; i < 15; i++){
-//     console.log(rocket1.acelerar());
-//     console.log(rocket2.acelerar());
-// }
-//     rocket1.info();
-//     rocket2.info();
+
+for (i=0; i < 5; i++){
+   rocket1.frenar();
+}
+for (i=0; i < 7; i++){
+    rocket2.acelerar();
+}
+    rocket1.info();
+    rocket2.info();
+
+for (i=0; i < 15; i++){
+    rocket1.acelerar();
+   rocket2.acelerar();
+}
+    rocket1.info();
+    rocket2.info();
