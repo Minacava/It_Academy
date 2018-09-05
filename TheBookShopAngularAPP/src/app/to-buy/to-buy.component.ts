bd0d0e3a2132a0abfe223e1e84ea2c5850f2d7ff
Minnacava <marinacamachovarea@outlook.com>
2018-09-05 22:29:40 +0200

    First Commit Angular Project

---
+++ TheBookShopAngularAPP/src/app/to-buy/to-buy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-buy',
+  templateUrl: './to-buy.component.html',
+  styleUrls: ['./to-buy.component.css']
+})
+export class ToBuyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
